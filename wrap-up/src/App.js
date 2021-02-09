c9010dcaa2c9ebff5ae9be197d18163290b3da94
Dana Gurland <dgurland@vt.edu>
2021-02-09 16:53:55 -0500

    Began dropdown menu

--- wrap-up/src/App.js
+++ wrap-up/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Menu, Dropdown } from 'antd';
+//import './App.css';
+import 'antd/dist/antd.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      menuItems:[],
+      isLoaded: false,
+    }
+  }
+
+
+  menu = () => {
+
+    const { SubMenu } = Menu;
+
+    let items = this.state.menuItems;
+    return (
+    <Menu>
+      {items.map((item, i)=> {
+        return(
+          <SubMenu title={item.name} key={i}>
+      <Menu.Item>3rd menu item</Menu.Item>
+      <Menu.Item>4th menu item</Menu.Item>
+    </SubMenu>
+        )
+      })}
+    </Menu>
   );
+    }
+  
+  componentDidMount() {
+    fetch(" https://xc-ajax-demo.herokuapp.com/api/countries/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            menuItems: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+
+  render(){
+    return(
+      this.state.isLoaded ? (
+    <div className="App">
+      <Dropdown className = "dropdown" overlay={this.menu()} placement="bottomLeft" trigger="click">
+      <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+      Dropdown Menu
+    </a>
+      </Dropdown>
+    </div> ) : <div><h1>Fetching data...</h1></div>
+    )
+  };
+
+
+ 
 }
 
 export default App;
